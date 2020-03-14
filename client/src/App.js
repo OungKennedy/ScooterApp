a21a07ec31ec2b5451cd86f8792bf6e5f60cbcd2
OungKennedy <oungkennedy@gmail.com>
2020-03-14 13:03:27 +0800

    removed title from app.jg

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Scooterapp from './components/Scooterapp';
 function App() {
   return (
     <div className="App">
-      <h1 className='title'>Scooter Search</h1>
       <Scooterapp/>
     </div>
   );
